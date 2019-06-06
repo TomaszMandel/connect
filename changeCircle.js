6d54679f976e08848ce1264066c0522a453fe1c7
Tomasz <tomasz.mandel@gmail.com>
2019-06-06 20:25:37 +0200

    start modyfiing color

--- changeCircle.js
+++ changeCircle.js
@@ -1,3 +1,11 @@
-const changecolor = () => {
-    
+// Check for current color.
+// TODO: change the color.
+// TODO: find a more concise way to express that.
+function changeColor() {
+    console.log("running");
+    if(document.getElementById("circle").style.backgroundColor = "#bf3eff") {
+        console.log("match");
+    } else {
+        console.log("no match")
+    }
 }
