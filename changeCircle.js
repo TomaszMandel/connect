8ee2859309431849b272f83eff9f4c9b063f6e7d
Tomasz <tomasz.mandel@gmail.com>
2019-06-07 21:27:37 +0200

    add helper functions to convert rgb to hex

--- changeCircle.js
+++ changeCircle.js
@@ -1,11 +1,29 @@
 // Check for current color.
 // TODO: change the color.
 // TODO: find a more concise way to express that.
-function changeColor() {
-    console.log("running");
-    if(document.getElementById("circle").style.backgroundColor = "#bf3eff") {
-        console.log("match");
-    } else {
-        console.log("no match")
-    }
+
+const colors = [
+    "#bf3eff",
+    "#fff68f",
+    "#a8251d",
+];
+
+console.log(colors);
+
+function getBackgroundColor() {
+    return window.getComputedStyle(circle).backgroundColor;
 }
+let kolor = getBackgroundColor()
+console.log(getBackgroundColor())
+
+
+
+// function changeColor() {
+    
+//     console.log("running");
+//     if(document.getElementById("circle").style.backgroundColor == "#bf3eff") {
+//         console.log("match");
+//     } else {
+//         console.log("no match")
+//     }
+// }
--- helpers.js
+++ helpers.js
@@ -5,3 +5,21 @@ function generateDisplay(id) {
     }
 }
 
+// From "rgb(r,g,b)" returns r,g,b as numbers.
+function rgbStrToInt(rgb) {
+    return rgb.replace("rgb(", "").replace(")", "")
+}
+
+
+//Convert rgb to hex.
+function componentToHex(c) {
+    let hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+// Converts rgb values to hex.
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+console.log(rgbToHex(191, 62, 255))
