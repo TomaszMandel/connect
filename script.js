afa348dbd90da4299a239b19307da7e5803525b5
Tomasz Mandel <tomasz.mandel@gmail.com>
2019-07-10 09:47:52 +0200

    App.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './App';
+
 console.log("Javascript is working and in fine mood")
+
+const USER = {
+    name: "Tomek", 
+    age: "222",
+}
+
+ReactDOM.render(<App user={USER} />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,21 @@ module.exports= {
     mode: 'development',
     output: {
         filename: 'bundle.js'
-    }
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react']
+                        }
+                    }
+                ],
+            }
+        ]
+    },
 }
